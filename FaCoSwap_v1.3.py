757494a7b3d854322b5c2485179f5fc877d66414
Nova Hadi Lestriandoko <adi.nur0611@gmail.com>
2023-02-21 04:10:59 +0100

    Update FaCoSwap_v1.3.py

--- FaCoSwap_v1.3.py
+++ FaCoSwap_v1.3.py
@@ -235,16 +235,19 @@ if __name__ == '__main__':
     st.write('---:eye:-:nose:-:lips:-:ear:-:eye:---')
             
     col1, col2 = st.columns(2)
-    if file1 and file2 and col1.button("Swap face 1") and swap_type=='Face parts':
-        if add_selectbox2=='NDTS' and status1 and status2 :
+    if file1 and file2 and col1.button("Swap face 1") :
+        if add_selectbox2=='NDTS' and status1 and status2 and swap_type=='Face parts' :
             output, d_img1, d_img2, mask, output_replacement, morphed_img  = ndts.LmPt_Morph(crop_img1, crop_img2, add_selectbox1, detector, predictor)
             st.image(output)
-        elif add_selectbox2=='NDT' and status1 and status2 :
+        elif add_selectbox2=='NDT' and status1 and status2 and swap_type=='Face parts' :
             output, morphed_img, mask_img, delaunay_img1, delaunay_img2, all_points, ori_points = fpr.face_part_replacement(crop_img1, detector, predictor, crop_img2, 1, add_selectbox1)
             st.image(output)
-        elif add_selectbox2=='NDS' and status1 and status2 :
+        elif add_selectbox2=='NDS' and status1 and status2 and swap_type=='Face parts' :
             output = NDS_morphing(crop_img1, crop_img2, predictor)
             st.image(output)
+        elif add_selectbox4=='whole face' and status1 and status2 and swap_type=='Whole face' :
+            output = wfs.wholeface_swap_1(img1, img2, detector, predictor)
+            st.image(output)            
         else:
             #st.image(Image.open(file2))
             if status1 and not status2:
@@ -264,6 +267,9 @@ if __name__ == '__main__':
         elif add_selectbox2=='NDS' and status1 and status2 :
             output = NDS_morphing(crop_img2, crop_img1, predictor)
             st.image(output)
+        elif add_selectbox4=='whole face' and status1 and status2 and swap_type=='Whole face' :
+            output = wfs.wholeface_swap_1(img2, img1, detector, predictor)
+            st.image(output)
         else:
             if status1 and not status2:
                 st.write("Faces required! Please upload face 2.")
@@ -272,34 +278,3 @@ if __name__ == '__main__':
             else:
                 st.write("Faces required! Please upload face 1 and face 2.")
         
-    if  file1 and file2 and col1.button("Swap face 1") and swap_type=='Whole face':
-        if add_selectbox4=='whole face' and status1 and status2 :
-            output = wfs.wholeface_swap_1(img1, img2, detector, predictor)
-            st.image(output)
-        elif add_selectbox4=='exclude beard area' and status1 and status2 :
-            st.image(crop_img1)
-        elif add_selectbox4=='exclude mouth and mustache area' and status1 and status2 :
-            st.image(crop_img1)
-        else:
-            if status1 and not status2:
-                st.write("Faces required! Please upload face 2.")
-            elif not status1 and status2:
-                st.write("Faces required! Please upload face 1.")
-            else:
-                st.write("Faces required! Please upload face 1 and face 2.")
-
-    if  file1 and file2 and col2.button("Swap face 2") and swap_type=='Whole face':
-        if add_selectbox4=='whole face' and status1 and status2 :
-            output = wfs.wholeface_swap_1(img2, img1, detector, predictor)
-            st.image(output)
-        elif add_selectbox4=='exclude beard area' and status1 and status2 :
-            st.image(crop_img1)
-        elif add_selectbox4=='exclude mouth and mustache area' and status1 and status2 :
-            st.image(crop_img1)
-        else:
-            if status1 and not status2:
-                st.write("Faces required! Please upload face 2.")
-            elif not status1 and status2:
-                st.write("Faces required! Please upload face 1.")
-            else:
-                st.write("Faces required! Please upload face 1 and face 2.")
