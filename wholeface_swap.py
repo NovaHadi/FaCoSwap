56e1ef0dcb658edfbb208406d389efdab956eeb8
Nova Hadi Lestriandoko <adi.nur0611@gmail.com>
2023-02-22 07:48:25 +0100

    Update wholeface_swap.py

--- wholeface_swap.py
+++ wholeface_swap.py
@@ -88,7 +88,7 @@ def wholeface_swap_1(img1, img2, _detector, _predictor):
 
     # ============ upgrade the quality of the whole face swap ==============
     morphed_landmark = manual_aligning_68_v3(img2, points2, points1)    
-    output_replacement, mask,  d_img1, d_img2 = mp.morphing_original(img1, morphed_landmark, d_img1, d_img2, mask_img, points1, points2, alpha=1)
+    output_replacement, mask,  d_img1, d_img2 = mp.morphing_original(img1, morphed_landmark, d_img1, d_img2, mask_img, points2, points1, alpha=1)
     
     center, src_mask = getMaskCenter(morphed_landmark, mask)
     output = cv2.seamlessClone(output_replacement, morphed_landmark, src_mask, center, cv2.NORMAL_CLONE)
